e0492303272bf881e9ba4b3ea6a546de7918d7f2
12345234 <KTC24A31A0066@edu.kyoto-tech.ac.jp>
2026-04-27 16:22:32 +0900

    パズルゲーム準備

---
+++ Assets/2dpazle/Scripts/SetTarget.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SetTarget : MonoBehaviour
+{
+    [SerializeField] private GameObject[] target;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < target.Length; i++)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
